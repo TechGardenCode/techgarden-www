e16e8a9325940d76694c680d30d35f4166a2f8d0
Tech Garden <techgardencode@gmail.com>
2025-08-25 13:17:18 -0400

    feat: posthog reverse proxy setup

--- projects/techgarden/src/environments/environment.development.ts
+++ projects/techgarden/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   POSTHOG_ENABLED: false,
   POSTHOG_KEY: 'phc_VNOdPTfW45O7rGYPKSMlLrp4uktQb1d9xOvwd7rll6c',
-  POSTHOG_HOST: 'https://us.i.posthog.com',
+  POSTHOG_HOST: 'https://oink.techgarden.gg',
 };
