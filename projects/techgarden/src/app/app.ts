1f2e741a485b593ffcdf3a2d8bfb6d630f9feae0
Tech Garden <techgardencode@gmail.com>
2025-08-26 17:03:11 -0400

    feat: add gesture support for opening mobile nav

--- projects/techgarden/src/app/app.ts
+++ projects/techgarden/src/app/app.ts
@@ -1,4 +1,12 @@
-import { Component, HostListener, inject, OnInit, signal } from '@angular/core';
+import {
+  Component,
+  Host,
+  HostBinding,
+  HostListener,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MobileService } from './services/mobile.service';
 import { Nav } from './components/shared/nav/nav';
@@ -17,7 +25,7 @@ import { Footer } from './components/shared/footer/footer';
   templateUrl: './app.html',
   styleUrl: './app.css',
   host: {
-    class: 'flex flex-col min-h-svh',
+    class: 'flex flex-col min-h-svh data-[mobile-open=true]:overflow-hidden',
   },
 })
 export class App {
@@ -43,6 +51,11 @@ export class App {
   }
   _scrollTimeout!: number;
 
+  @HostBinding('attr.data-mobile-open')
+  get isMobileNavOpen(): boolean {
+    return this.mobileService.isNavOpen();
+  }
+
   @HostListener('window:keydown.escape')
   onEscapeKeyDown(): void {
     this.mobileService.closeNav();
@@ -53,6 +66,51 @@ export class App {
     const shouldShow = window.scrollY > 0;
     if (this.pageScrolledY() !== shouldShow) {
       this.pageScrolledY.set(shouldShow);
+    }
+  }
+
+  private swipeCoord = signal<[number, number] | null>(null);
+  private swipeTime = signal<number | null>(null);
+
+  @HostListener('window:touchstart', ['$event'])
+  onTouchStart(event: TouchEvent): void {
+    const coord: [number, number] = [
+      event.changedTouches[0].clientX,
+      event.changedTouches[0].clientY,
+    ];
+    const time = new Date().getTime();
+    this.swipeCoord.set(coord);
+    this.swipeTime.set(time);
+  }
+
+  @HostListener('window:touchend', ['$event'])
+  onTouchEnd(event: TouchEvent): void {
+    const coord: [number, number] = [
+      event.changedTouches[0].clientX,
+      event.changedTouches[0].clientY,
+    ];
+    const time = new Date().getTime();
+    const swipeCoord = this.swipeCoord();
+    if (!swipeCoord) return;
+    const swipeTime = this.swipeTime();
+    if (!swipeTime) return;
+    const duration = time - swipeTime;
+    const direction = [coord[0] - swipeCoord[0], coord[1] - swipeCoord[1]];
+
+    if (
+      duration < 1000 &&
+      Math.abs(direction[0]) > 30 &&
+      Math.abs(direction[0]) > Math.abs(direction[1] * 3)
+    ) {
+      if (direction[0] < 0) {
+        if (swipeCoord[0] > window.innerWidth / 2) {
+          this.mobileService.closeNav();
+        }
+      } else {
+        if (swipeCoord[0] < window.innerWidth / 2) {
+          this.mobileService.openNav();
+        }
+      }
     }
   }
 
