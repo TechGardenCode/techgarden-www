d5ecb140bad520197f61e50473734db78052e03e
Tech Garden <techgardencode@gmail.com>
2025-08-29 23:30:09 -0400

    feat: remove guard from admin (should be handled by bff)

--- projects/techgarden/src/app/app.routes.ts
+++ projects/techgarden/src/app/app.routes.ts
@@ -21,7 +21,6 @@ export const routes: Routes = [
     path: 'admin',
     loadChildren: () =>
       import('./pages/admin/admin-module').then((m) => m.AdminModule),
-    canActivate: [() => !environment.production],
   },
   {
     path: '**',
